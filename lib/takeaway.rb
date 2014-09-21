9ff5aa479f2575c86db6f38767579cb220b72042
javier <js@silveriofunk.com>
2014-09-21 21:52:32 +0100

    all classes added

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,13 +1,16 @@
 class Takeaway
 
-attr_reader :menu
+	attr_reader :menu
+	attr_reader :order
 
-def initialize
-	@menu = true
-end
+	def initialize
+		@menu = true
+		@order = []
+	end
 
-def menu?
-	@menu
-end
+	def has_menu?
+		@menu
+	end
+
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -5,11 +5,7 @@ describe Takeaway do
 		let(:theDiner) { Takeaway.new }
 
 		it 'has a menu when initialized' do
-		expect(theDiner.menu?).to be true
+				expect(theDiner.has_menu?).to be true
 		end
-
-		# it 'can take orders from customers' do
-		# 	allow(theDiner).to receive(:order) {"Una de albondigas"}
-		# end
 
 	end

195d9f7d43d3aaebb33d892b138194085e03bebc
silver-io <javier@silver.io>
2014-11-15 23:42:06 +0000

    line_item, now able to add more than one dish

--- spec/line_item_spec.rb
+++ spec/line_item_spec.rb
@@ -1,12 +1,17 @@
 require 'line_item'
 
 describe LineItem do
-  let (:LineItem) {LineItem.new(:dish, 1)}
+  let (:LineItem) {LineItem.new(:pasta, 1)}
 
         it 'is initialized with a dish and quantity' do
           pasta = double :dish
           expect {LineItem.new(pasta, 4)}.not_to raise_error
         end
+
+        it 'can have a number of dishes' do
+          item = LineItem.new(:pasta, 3)
+          expect(item.quantity).to eq (3)
+  end
 
   end
 
